3a588c2151e4d5521a69e55ba32906eb3c846743
01HB <bspaceship3@gmail.com>
2024-05-22 05:05:40 +0600

    initial commit with the setup(react+tailwind)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+
+  return (
+    <>
+      <div className="w-full min-h-screen h-fit flex justify-center bg-white">
+        <div className="w-full max-w-[1800px] flex flex-col items-center">
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  
+  plugins: [],
+}
+
